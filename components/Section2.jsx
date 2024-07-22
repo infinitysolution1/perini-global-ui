87ccdccf1c2a7daca218aa688d7a1ccde31dcf02
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-07-22 18:56:00 +0530

    added section 2 and 3

---
+++ components/Section2.jsx
@@ -0,0 +1,197 @@
+"use client";
+import React, { useRef, useEffect, useState } from "react";
+import Image from "next/image";
+import { motion, useTransform, useScroll } from "framer-motion";
+import RevealOnScroll from "./RevealOnScroll";
+
+const title = "DISCOVER";
+const title2 = "NEW FABRICS";
+
+const HomeSection2 = (ref) => {
+  const [scrolled, setScrolled] = useState(false);
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+
+  const x = useTransform(scrollYProgress, [0, 1], ["-20%", "20%"]);
+
+  useEffect(() => {
+    if (scrollYProgress.current > 0.5) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  }, [scrollYProgress]);
+
+  return (
+    <section className="relative h-full lg:h-[100vh] bg-[#FDF8F2] w-full overflow-hidden">
+      <div className=" w-full flex flex-row justify-center h-full">
+        <div className=" flex flex-col items-center w-1/4">
+          <motion.div style={{ y: x }} className="flex flex-col gap-y-[5vh]">
+            {[1, 2, 3, 4, 5, 1, 2, 3, 4, 5].map((cycle, index) => {
+              return (
+                <div
+                  key={index}
+                  className={` flex flex-col h-full w-[15vh]  animate-bounce `}
+                >
+                  <div
+                    className={`relative h-full w-full bg-[#EFE6DA] rounded-lg shadow-md ${
+                      index % 2 == 0
+                        ? "transform rotate-6"
+                        : "tranform -rotate-6"
+                    }`}
+                  >
+                    <div className="relative h-[15vh] w-full rounded-lg overflow-hidden">
+                      <Image
+                        src={`/fabric${cycle}.png`}
+                        layout="fill"
+                        objectFit="cover"
+                      />
+                    </div>
+                    <div className="flex flex-col items-start justify-center px-4 py-4">
+                      <h3 className="text-[#392E26] text-lg font-semibold">
+                        Fabric {cycle}
+                      </h3>
+                      <p className="text-[#392E26] text-xs">PRINT</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </motion.div>
+        </div>
+        <div className=" flex flex-col items-center justify-center ">
+          <RevealOnScroll
+            threshold={0.5}
+            addedClasses=" flex flex-col w-full  items-center text-center"
+          >
+            <h2 className=" text-[6rem] text-[#392E26]  overflow-hidden leading-[5.5rem] mt-[5vh]">
+              {title.match(/./gu).map((char, index) => (
+                <span
+                  className="animate-text-reveal inline-block  [animation-fill-mode:backwards]"
+                  key={`${char}-${index}`}
+                  style={{ animationDelay: `${2 + index * 0.05}s` }}
+                >
+                  {char === " " ? "\u00A0" : char}
+                </span>
+              ))}
+            </h2>
+            <h2 className=" text-[6rem] text-[#392E26]  overflow-hidden leading-[5.5rem] mt-[2.5vh]">
+              {title2.match(/./gu).map((char, index) => (
+                <span
+                  className="animate-text-reveal inline-block [--slidein-delay:1000ms] [animation-fill-mode:backwards]"
+                  key={`${char}-${index}`}
+                  style={{ animationDelay: `${2 + index * 0.05}s` }}
+                >
+                  {char === " " ? "\u00A0" : char}
+                </span>
+              ))}
+            </h2>
+          </RevealOnScroll>
+        </div>
+        <div className=" flex flex-col items-center w-1/4">
+          <motion.div style={{ y: x }} className="flex flex-col gap-y-[5vh]">
+            {[4, 5, 1, 2, 3, 4, 5, 1, 2, 3].map((cycle, index) => {
+              return (
+                <div
+                  key={index}
+                  className={` flex flex-col h-full w-[15vh]  animate-bounce `}
+                >
+                  <div
+                    className={`relative h-full w-full bg-[#EFE6DA] rounded-lg shadow-md ${
+                      index % 2 == 0
+                        ? "transform rotate-6"
+                        : "tranform -rotate-6"
+                    }`}
+                  >
+                    <div className="relative h-[15vh] w-full rounded-lg overflow-hidden">
+                      <Image
+                        src={`/fabric${cycle}.png`}
+                        layout="fill"
+                        objectFit="cover"
+                      />
+                    </div>
+                    <div className="flex flex-col items-start justify-center px-4 py-4">
+                      <h3 className="text-[#392E26] text-lg font-semibold">
+                        Fabric {cycle}
+                      </h3>
+                      <p className="text-[#392E26] text-xs">PRINT</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </motion.div>
+        </div>
+      </div>
+      <div ref={targetRef} className=" w-full " />
+      {/* <div className={` sticky left-0 top-[25vh] w-full z-0 `}>
+        <RevealOnScroll
+          threshold={0.5}
+          addedClasses=" flex flex-col w-full  items-center text-center"
+        >
+          <h2 className=" text-[7rem] text-[#392E26]  overflow-hidden leading-[6.5rem] mt-[5vh]">
+            {title.match(/./gu).map((char, index) => (
+              <span
+                className="animate-text-reveal inline-block [animation-fill-mode:backwards]"
+                key={`${char}-${index}`}
+                style={{ animationDelay: `${1 + index * 0.05}s` }}
+              >
+                {char === " " ? "\u00A0" : char}
+              </span>
+            ))}
+          </h2>
+          <h2 className=" text-[7rem] text-[#392E26]  overflow-hidden leading-[6.5rem] mt-[2.5vh]">
+            {title2.match(/./gu).map((char, index) => (
+              <span
+                className="animate-text-reveal inline-block [animation-fill-mode:backwards]"
+                key={`${char}-${index}`}
+                style={{ animationDelay: `${1 + index * 0.05}s` }}
+              >
+                {char === " " ? "\u00A0" : char}
+              </span>
+            ))}
+          </h2>
+        </RevealOnScroll>
+      </div>
+      <div className="sticky top-[12.5vh]  hidden lg:flex flex-col h-screen items-center overflow-hidden">
+        <motion.div
+          style={{ x }}
+          className="flex flex-row gap-x-[25vh] mt-[15vh]"
+        >
+          {[1, 2, 3, 4, 5].map((cycle, index) => {
+            return (
+              <div
+                key={index}
+                className={` flex flex-col h-full w-[45vh]  animate-bounce `}
+              >
+                <div
+                  className={`relative h-full w-full bg-[#EFE6DA] rounded-lg shadow-md ${
+                    index % 2 == 0 ? "transform rotate-6" : "tranform -rotate-6"
+                  }`}
+                >
+                  <div className="relative h-[45vh] w-full rounded-lg overflow-hidden">
+                    <Image
+                      src={`/fabric${cycle}.png`}
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </div>
+                  <div className="flex flex-col items-start justify-center px-4 py-4">
+                    <h3 className="text-[#392E26] text-3xl font-semibold">
+                      Fabric {cycle}
+                    </h3>
+                    <p className="text-[#392E26] text-base">PRINT</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </motion.div>
+      </div> */}
+    </section>
+  );
+};
+
+export default HomeSection2;
---
+++ components/Section3.jsx
@@ -0,0 +1,132 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import RevealOnScroll from "./RevealOnScroll";
+import useStore from "@/utils/store";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
+import { fabrics } from "@/utils/consts";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+const colors = ["bg-[#702C28]", "bg-[#392E26]", "bg-[#788668]"];
+
+const HomeSection3 = () => {
+  const [refresh, setRefresh] = useState(false);
+  const [currentColor, setCurrentColor] = useState(0);
+  const [flip, setFlip] = useState(false);
+
+  const { alt, setAlt } = useStore();
+
+  return (
+    <div
+      className={`w-screen h-full lg:h-screen flex flex-col transition-all duration-[600ms] ${colors[currentColor]} items-center  py-8 lg:py-0`}
+    >
+      <div className="flex flex-col items-center w-full  h-[100vh] ">
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          speed={2000}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+            duration: 2,
+          }}
+          loop={true}
+          onSlideChange={() =>
+            setCurrentColor((currentColor + 1) % colors.length)
+          }
+          modules={[Autoplay, Navigation]}
+          className="mySwiper  h-full w-full -z-10 "
+        >
+          {fabrics.map((item, index) => {
+            return (
+              <SwiperSlide
+                key={index}
+                className="h-[100vh] w-screen flex relative"
+              >
+                <div
+                  className={`h-full md:h-[100vh] relative w-screen flex flex-row justify-between items-center `}
+                >
+                  <div className=" flex flex-col items-center w-1/4">
+                    <RevealOnScroll
+                      addedClasses={`flex flex-col h-[50vh] w-[50vh] animate-slideInLeft `}
+                    >
+                      <div>
+                        <div className="relative h-full w-full transform rotate-[35deg]  overflow-hidden  rounded-[5vw] shadow-lg ">
+                          <img
+                            src={`${item.image1}`}
+                            className="h-full w-full object-cover"
+                          />
+                        </div>
+                      </div>
+                    </RevealOnScroll>
+                  </div>
+                  <div className=" flex flex-col items-center justify-center w-1/2">
+                    <p className=" text-xl border-b-[0.5px] border-white ">
+                      {item.subtitle}
+                    </p>
+                    <h2 className=" text-[7rem]  overflow-hidden leading-[6.5rem] mt-[5vh]">
+                      {item.title
+                        .split(" ")[0]
+                        .match(/./gu)
+                        .map((char, index) => (
+                          <span
+                            className="animate-text-reveal inline-block [animation-fill-mode:backwards]"
+                            key={`${char}-${index}`}
+                            style={{ animationDelay: `${1 + index * 0.05}s` }}
+                          >
+                            {char === " " ? "\u00A0" : char}
+                          </span>
+                        ))}
+                    </h2>
+                    <h2 className=" text-[7rem]  overflow-hidden leading-[6.5rem] mt-[2.5vh]">
+                      {item.title
+                        .split(" ")[1]
+                        .match(/./gu)
+                        .map((char, index) => (
+                          <span
+                            className="animate-text-reveal inline-block [animation-fill-mode:backwards]"
+                            key={`${char}-${index}`}
+                            style={{ animationDelay: `${1 + index * 0.05}s` }}
+                          >
+                            {char === " " ? "\u00A0" : char}
+                          </span>
+                        ))}
+                    </h2>
+                  </div>
+                  <div className=" flex flex-col items-center w-1/4">
+                    <RevealOnScroll
+                      addedClasses={`flex flex-col h-[50vh] w-[50vh]  animate-slideInRight `}
+                    >
+                      <div>
+                        <div className="relative h-full w-full transform -rotate-[35deg]  overflow-hidden  rounded-[5vw] shadow-lg ">
+                          <img
+                            src={`${item.image2}`}
+                            className="h-full w-full object-cover"
+                          />
+                        </div>
+                      </div>
+                    </RevealOnScroll>
+                  </div>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+
+      <RevealOnScroll
+        threshold={0.5}
+        refresh={() => {
+          setRefresh(!refresh);
+        }}
+        className=" absolute bottom-0 flex flex-col w-full h-1"
+      />
+    </div>
+  );
+};
+
+export default HomeSection3;
